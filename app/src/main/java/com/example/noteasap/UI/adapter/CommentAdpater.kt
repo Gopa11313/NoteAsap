0f8d9b7b87df017ac599bdfd59da553c1059878e
Gopa11313 <gopalaa9819525993@gmail.com>
2021-01-19 19:01:05 +0545

    done with comment pop up

--- app/src/main/java/com/example/noteasap/UI/adapter/CommentAdpater.kt
+++ app/src/main/java/com/example/noteasap/UI/adapter/CommentAdpater.kt
@@ -1,12 +1,19 @@
 package com.example.noteasap.UI.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteasap.R
+import com.example.noteasap.UI.pupup.EditProfileActivity
+import com.example.noteasap.UI.pupup.MessageActivity
+import kotlin.coroutines.coroutineContext
 
 class CommentAdpater(
     val listcomment:ArrayList<com.example.noteasap.UI.model.Comment>,
@@ -15,9 +22,34 @@ class CommentAdpater(
     class CommentviewHolder(view:View):RecyclerView.ViewHolder(view){
         val name:TextView;
         val comment:TextView;
+        val update_delete:ImageButton;
         init {
             name=view.findViewById(R.id.name)
             comment=view.findViewById(R.id.comment)
+            update_delete=view.findViewById(R.id.imageView4)
+            update_delete.setOnClickListener(){
+                val popupMenu= PopupMenu(this, it)
+                popupMenu.menuInflater.inflate(R.menu.menu2,popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener { iteam->
+                    when(iteam.itemId){
+                        R.id.edit ->{
+                            requireActivity().run{
+                            }
+                            true
+                        }
+                        R.id.message -> {
+                            requireActivity().run{
+                            }
+                            true
+                        }
+                        R.id.About->{
+                            true
+                        }
+                        else -> false
+                    }
+                }
+                popupMenu.show()
+            }
         }
 
     }
