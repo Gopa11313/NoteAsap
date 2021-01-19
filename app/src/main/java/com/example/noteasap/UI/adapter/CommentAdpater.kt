1e5db31fb44de00f83e00211a0b683e8d29f7825
Gopa11313 <gopalaa9819525993@gmail.com>
2021-01-19 19:29:46 +0545

    pop up

--- app/src/main/java/com/example/noteasap/UI/adapter/CommentAdpater.kt
+++ app/src/main/java/com/example/noteasap/UI/adapter/CommentAdpater.kt
@@ -1,8 +1,6 @@
 package com.example.noteasap.UI.adapter
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,15 +9,14 @@ import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteasap.R
-import com.example.noteasap.UI.pupup.EditProfileActivity
-import com.example.noteasap.UI.pupup.MessageActivity
-import kotlin.coroutines.coroutineContext
+
 
 class CommentAdpater(
-    val listcomment:ArrayList<com.example.noteasap.UI.model.Comment>,
-    val context: Context):RecyclerView.Adapter<CommentAdpater.CommentviewHolder>()
+    val listcomment: ArrayList<com.example.noteasap.UI.model.Comment>,
+    val context: Context,
+):RecyclerView.Adapter<CommentAdpater.CommentviewHolder>()
 {
-    class CommentviewHolder(view:View):RecyclerView.ViewHolder(view){
+    class CommentviewHolder(view: View):RecyclerView.ViewHolder(view){
         val name:TextView;
         val comment:TextView;
         val update_delete:ImageButton;
@@ -28,35 +25,19 @@ class CommentAdpater(
             comment=view.findViewById(R.id.comment)
             update_delete=view.findViewById(R.id.imageView4)
             update_delete.setOnClickListener(){
-                val popupMenu= PopupMenu(this, it)
-                popupMenu.menuInflater.inflate(R.menu.menu2,popupMenu.menu)
-                popupMenu.setOnMenuItemClickListener { iteam->
-                    when(iteam.itemId){
-                        R.id.edit ->{
-                            requireActivity().run{
-                            }
-                            true
-                        }
-                        R.id.message -> {
-                            requireActivity().run{
-                            }
-                            true
-                        }
-                        R.id.About->{
-                            true
-                        }
-                        else -> false
-                    }
-                }
+                val popupMenu = PopupMenu(view.getContext(), view)
+                popupMenu.inflate(R.menu.menu3)
+//                popupMenu.setOnMenuItemClickListener()
                 popupMenu.show()
             }
+
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentviewHolder {
         val view=LayoutInflater.from(parent.context)
-            .inflate(R.layout.comment,parent,false)
+            .inflate(R.layout.comment, parent, false)
         return CommentviewHolder(view)
     }
 
@@ -69,5 +50,8 @@ class CommentAdpater(
 
     override fun getItemCount(): Int {
         return listcomment.size;
+    }
+    fun popupmenu(){
+
     }
 }
