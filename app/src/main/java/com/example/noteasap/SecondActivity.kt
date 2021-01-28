92e84213175c24ca67855c4f39f5d4be766088ea
Gopa11313 <gopalaa9819525993@gmail.com>
2021-01-28 10:16:43 +0545

    Finished permisiion and shared preference

--- app/src/main/java/com/example/noteasap/SecondActivity.kt
+++ app/src/main/java/com/example/noteasap/SecondActivity.kt
@@ -1,19 +1,27 @@
 package com.example.noteasap
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.ActivityCompat
 import com.example.noteasap.UI.thirdActivity.Third_Activity
 
 private lateinit var notif: Button;
 var flag=false;
 class SecondActivity : AppCompatActivity() {
+    private val permissions = arrayOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         notif=findViewById(R.id.notif_Button);
-
+        if (!hasPermission()) {
+            requestPermission()
+        }
         notif.setOnClickListener(){
             flag=true;
             nexatact();
@@ -22,5 +30,24 @@ class SecondActivity : AppCompatActivity() {
     fun nexatact(){
         startActivity(Intent(this, Third_Activity::class.java))
         finish();
+    }
+    fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this@SecondActivity,
+            permissions, 876
+        )
+    }
+    fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+            }
+        }
+        return hasPermission
     }
 }
--- app/src/main/java/com/example/noteasap/SplashActivity.kt
+++ app/src/main/java/com/example/noteasap/SplashActivity.kt
@@ -20,60 +20,36 @@ private lateinit var note:TextView;
 private lateinit var fadein:Animation;
 private lateinit var slogan:TextView;
 class SplashActivity : AppCompatActivity() {
-    private val permissions = arrayOf(
-        android.Manifest.permission.CAMERA,
-        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        note=findViewById(R.id.note)
-        slogan=findViewById(R.id.slogan)
-        fadein=AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        note = findViewById(R.id.note)
+        slogan = findViewById(R.id.slogan)
+        fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in)
         note.startAnimation(fadein)
         slogan.startAnimation(fadein)
 
         CoroutineScope(Dispatchers.IO).launch {
             delay(2000)
-            withContext(Main){
-                if (!hasPermission()) {
-                    requestPermission()
-                    stratactivity()
-                }
+            withContext(Main) {
+
+                stratactivity()
             }
         }
     }
-    fun stratactivity(){
+
+    fun stratactivity() {
         val intent = Intent(this, SecondActivity::class.java)
         startActivity(intent)
         finish()
     }
 
-    fun getSharedPref(){
-        val sharedPref=getSharedPreferences("MyPref", MODE_PRIVATE)
-        val username=sharedPref.getString("username","")
-        val password=sharedPref.getString("password","")
+    fun getSharedPref() {
+        val sharedPref = getSharedPreferences("MyPref", MODE_PRIVATE)
+        val username = sharedPref.getString("username", "")
+        val password = sharedPref.getString("password", "")
         Toast.makeText(this, "Username:$username and Password:$password", Toast.LENGTH_SHORT).show()
     }
 
-    fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            this@SplashActivity,
-            permissions, 876
-        )
-    }
-    fun hasPermission(): Boolean {
-        var hasPermission = true
-        for (permission in permissions) {
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    permission
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                hasPermission = false
-            }
-        }
-        return hasPermission
-    }
 }
 
