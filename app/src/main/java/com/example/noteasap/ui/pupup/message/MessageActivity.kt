6c7b97815f601a6ae8f09faf7132ab6c478ea631
Gopa11313 <gopalaa9819525993@gmail.com>
2021-01-29 13:06:22 +0545

    binding view and viewmodel in activity

--- app/src/main/java/com/example/noteasap/ui/pupup/message/MessageActivity.kt
+++ app/src/main/java/com/example/noteasap/ui/pupup/message/MessageActivity.kt
@@ -2,11 +2,22 @@ package com.example.noteasap.ui.pupup.message
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.noteasap.R
+import com.example.noteasap.databinding.ActivityLoginBinding
+import com.example.noteasap.ui.login.LoginViewModel
 
 class MessageActivity : AppCompatActivity() {
+    private lateinit var message:EditText;
+    private lateinit var loginViewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_message)
+       val binding:ActivityLoginBinding=DataBindingUtil.setContentView(this,R.layout.activity_message)
+        binding.lifecycleOwner=this
+        message=findViewById(R.id.message)
+        loginViewModel=ViewModelProvider(this).get(LoginViewModel::class.java)
+        binding.loginViewModel=loginViewModel
     }
 }
