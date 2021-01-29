f6e71eabd58d4fe476cd4b9018688b8214579253
Gopa11313 <gopalaa9819525993@gmail.com>
2021-01-29 11:36:53 +0545

    livedata in LoginViewmodel

--- app/src/main/java/com/example/noteasap/ui/login/loginViewModel.kt
+++ app/src/main/java/com/example/noteasap/ui/login/loginViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.noteasap.ui.login
 
-class loginViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class loginViewModel:ViewModel() {
+
+    val _email=MutableLiveData<String>()
+    val _password=MutableLiveData<String>()
+
+    val email:LiveData<String>
+        get() = _email
+
+    val password:LiveData<String>
+        get()=_password
 }
