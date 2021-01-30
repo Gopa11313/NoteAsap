22b2df5ede2a12a7eaf0d7bbf07089c4868d9f63
Gopa11313 <gopalaa9819525993@gmail.com>
2021-01-30 19:27:03 +0545

    Done with contentviewModel

--- app/src/main/java/com/example/noteasap/ui/content/ContentviewModel.kt
+++ app/src/main/java/com/example/noteasap/ui/content/ContentviewModel.kt
@@ -1,4 +1,43 @@
 package com.example.noteasap.ui.content
 
-class ContentviewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ContentviewModel:ViewModel() {
+
+    val _topic= MutableLiveData<String>()
+
+    val _uniName =MutableLiveData<String>()
+
+    val _dis=MutableLiveData<String>()
+
+    val _rating =MutableLiveData<Int>()
+
+    val _cmnt =MutableLiveData<String>()
+
+
+
+    val topic:LiveData<String>
+        get() = _topic
+
+    val uniName:LiveData<String>
+        get()=_uniName
+
+    val dis:LiveData<String>
+        get() = _dis
+
+    val rating:LiveData<Int>
+        get()=_rating
+
+    val cmnt:LiveData<String>
+        get() = _cmnt
+
+
+//    var Setrating:LiveData<Int>
+//        get() {
+//            TODO()
+//        }
+//        set()=_rating
+
 }
