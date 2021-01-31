5047158ef841d17660450d4720c49872aac0335f
Gopa11313 <gopalaa9819525993@gmail.com>
2021-01-31 10:52:52 +0545

    Viewmode of HomeFragment

--- app/src/main/java/com/example/noteasap/ui/fragments/homeFragment/HomeFragmetViewModel.kt
+++ app/src/main/java/com/example/noteasap/ui/fragments/homeFragment/HomeFragmetViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.noteasap.ui.fragments.homeFragment
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 
-class homeFragmetViewModel:ViewModel() {
+class HomeFragmetViewModel:ViewModel() {
     val _autocmptText =MutableLiveData<String>()
+
+    val autocmptText:LiveData<String>
+        get() = _autocmptText
+
 
 }
