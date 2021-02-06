9e6bcbe607eba123be677de11cdc926f890b5f07
Gopa11313 <gopalaa9819525993@gmail.com>
2021-02-06 19:27:26 +0545

    Done with bookmarkFragment Viewmodel

--- app/src/main/java/com/example/noteasap/ui/fragments/bookmarkFragment/BookMarkFragmentViewModel.kt
+++ app/src/main/java/com/example/noteasap/ui/fragments/bookmarkFragment/BookMarkFragmentViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.noteasap.ui.fragments.bookmarkFragment
 
-class BookMarkFragmentViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BookMarkFragmentViewModel:ViewModel() {
+    val _search=MutableLiveData<String>()
+
+    val search:LiveData<String>
+        get() = _search
 }
