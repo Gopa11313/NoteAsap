e0cffd0018bd75627cabaae1cc52b27e9eae5032
Gopa11313 <gopalaa9819525993@gmail.com>
2021-02-07 12:15:53 +0545

    Done with MyApiRequest

--- app/src/main/java/com/example/noteasap/api/SignupApi.kt
+++ app/src/main/java/com/example/noteasap/api/SignupApi.kt
@@ -4,7 +4,7 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface SignupApi {
-    @Headers("Content-Type: application/json")
+
     @POST("/insert")
     fun registerUser(@Body map: HashMap<String, String>): Call<Void?>?
 
