cf89bc4ca08eb8e245ea98aa926aa81163c85262
gopa11313 <gopalaa9819525993@gmail.com>
2021-03-13 22:22:07 +0545

    done 2 testing

--- app/src/test/java/com/example/noteasap/NoteAsapTesting.kt
+++ app/src/test/java/com/example/noteasap/NoteAsapTesting.kt
@@ -11,37 +11,33 @@ class NoteAsapTesting {
     private lateinit var repository: UserRepository;
 
     @Test
-    fun checkRegisterUSer()= runBlocking{
-        val user=User(name = "this is test",email = "thisisemail@gmail.com",password = "testing12")
-        repository= UserRepository()
-        val expectedResult=true
-        val response=repository.registerUser(user)
-        val actualResult=response.success!!
-        Assert.assertEquals(expectedResult,actualResult)
+    fun checkRegisterUSer() = runBlocking {
+        val user =
+            User(name = "this is test", email = "thisisemail1212@gmail.com", password = "testing12")
+        repository = UserRepository()
+        val expectedResult = true
+        val response = repository.registerUser(user)
+        val actualResult = response.success!!
+        Assert.assertEquals(expectedResult, actualResult)
 
-        }
+    }
+
     @Test
-    fun loginUser()= runBlocking{
-        val user=User(email = "gopal@gmail.com",password = "gopal123")
-        repository= UserRepository()
-        val expectedResult=true
-        val response=repository.checkUSer(user)
-        val actualResult=response.success
-        Assert.assertEquals(expectedResult,actualResult)
+    fun loginUser() = runBlocking {
+        val user = User(email = "gopal@gmail.com", password = "gopal123")
+        repository = UserRepository()
+        val expectedResult = true
+        val response = repository.checkUSer(user)
+        val actualResult = response.success
+        Assert.assertEquals(expectedResult, actualResult)
     }
 
+
+    //-----------------------------Student Testing-----------------------------//
+    @Test
+    fun getme()= runBlocking{
+        val repository=UserRepository()
+        val response = repository.getme()
     }
 
-// -----------------------------Student Testing-----------------------------
-//@Test
-//fun addStudent() = runBlocking {
-//    userRepository = UserRepository()
-//    val student =
-//        Student(fullname = "fullName", age = 33, gender = "gender", address = "address")
-//    studentRepository = StudentRepository()
-//    ServiceBuilder.token ="Bearer " + userRepository.checkUser("kiran","kiran123").token
-//    val expectedResult = true
-//    val actualResult = studentRepository.insertStudent(student).success
-//    Assert.assertEquals(expectedResult, actualResult)
-//}
-//}
+}
